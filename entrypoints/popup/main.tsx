2bd68209f41003e5703e9a1fc86b46f443fcb4a0
“pinion05” <shalompmc0505@naver.com>
2026-01-20 23:32:54 +0900

    refactor: convert popup to React (main.tsx, App.tsx)

--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -1,6 +1,11 @@
-import { render } from 'solid-js/web';
-
-import './style.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import './style.css';
 
-render(() => <App />, document.getElementById('root')!);
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
